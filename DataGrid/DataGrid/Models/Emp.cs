9cd3862e25b713393a7895037caafde89b780db8
vipstar <vipstar_d@naver.com>
2022-09-28 18:45:42 +0900

    datagrid

---
+++ DataGrid/DataGrid/Models/Emp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid.Models
+{
+    public class Emp
+    {
+        private int _Empno;
+        private string _EName;
+        private int _Deptno;
+
+        public int Empno
+        {
+            get{return _Empno; }
+            set { _Empno = value; }
+        }
+
+        public string Ename
+        {
+            get { return _EName; }
+            set { _EName = value; }
+        }
+
+        public int Deptno
+        {
+            get { return _Deptno; }
+            set { _Deptno = value; }
+        }
+        public  Emp(int Empno, string Ename, int Deptno)
+        {
+            this.Empno = Empno;
+            this.Ename = Ename;
+            this._Deptno = Deptno;
+        }
+    }
+}
--- ProcessGet/ConsoleApp1/Program.cs
+++ ProcessGet/ConsoleApp1/Program.cs
@@ -11,43 +11,42 @@ namespace ConsoleApp1
     
         static void Main(string[] args)
         {
-            PerformanceCounter PC;
+            //PerformanceCounter PC;
 
             while (true)
             {
 
 
 
-                Process[] allProcesses = Process.GetProcesses();
-                Process pro = Process.GetCurrentProcess(); // 현재 프로세스 사용량
-                 PC = new PerformanceCounter("Process", "Working Set - Private", "DWConfig");
+            //    Process[] allProcesses = Process.GetProcesses();
+            //    PC = new PerformanceCounter("Process", "Working Set - Private", "DWConfig");
                  
-                long memory1 = 0;
+            //    long memory1 = 0;
 
-                foreach (Process p in allProcesses) memory1 += p.VirtualMemorySize64;
+            //    foreach (Process p in allProcesses) memory1 += p.VirtualMemorySize64;
 
-            Console.WriteLine($"RAM : {memory1} ");
+            //Console.WriteLine($"RAM : {memory1} ");
 
-            /* 현재 프로세스 메모리 사용량 가져오기*/
-             double memsize = Convert.ToDouble(PC.NextValue()) / (double)1024;
-             double memsize_MB =Math.Round(Convert.ToDouble(PC.NextValue()) / ((double)1024 * (double)1024), 1);
-             double memsize_GB = memsize_MB / (double)1024;
+            ///* 현재 프로세스 메모리 사용량 가져오기*/
+            // double memsize = Convert.ToDouble(PC.NextValue()) / (double)1024;
+            // double memsize_MB =Math.Round(Convert.ToDouble(PC.NextValue()) / ((double)1024 * (double)1024), 1);
+            // double memsize_GB = memsize_MB / (double)1024;
 
         
-             Console.WriteLine("DWConfig : {0:#,###} KB", memsize );
-             Console.WriteLine("DWConfig : {0:#,###} MB", memsize_MB);
-             Console.WriteLine("DWConfig : {0:0.000} GB", memsize_GB);
+            // Console.WriteLine("DWConfig : {0:#,###} KB", memsize );
+            // Console.WriteLine("DWConfig : {0:#,###} MB", memsize_MB);
+            // Console.WriteLine("DWConfig : {0:0.000} GB", memsize_GB);
 
-             GetTotalUsedMemory(memsize_MB);
+             GetTotalUsedMemory();
 
 
-                int milliseconds = 10000;
-             Thread.Sleep(milliseconds);
+                   int milliseconds = 10000;
+                Thread.Sleep(milliseconds);
 
             }
         }
 
-        public static void GetTotalUsedMemory(double memsize_MB)
+        public static void GetTotalUsedMemory(/*double memsize_MB*/)
         {
             ManagementClass cls = new ManagementClass("Win32_OperatingSystem");
             ManagementObjectCollection instances = cls.GetInstances();
@@ -55,40 +54,32 @@ namespace ConsoleApp1
             foreach (ManagementObject info in instances)
             {
                 double total_physical_memeory = double.Parse(info["TotalVisibleMemorySize"].ToString());
-                double total_physical_memeory_MB = total_physical_memeory /   (double)1024;
-                double total_physical_memeory_GB = total_physical_memeory / ((double)1024 * (double)1024);
                 double free_physical_memeory = double.Parse(info["FreePhysicalMemory"].ToString());
                 double remain_physical_memory = total_physical_memeory - free_physical_memeory;
-                double remain_physical_memory_MB = (total_physical_memeory - free_physical_memeory)/1024;
 
                 Console.WriteLine("Memory Information ================================");
-
-                Console.WriteLine("Total Physical Memory :{0:#.###} GB", total_physical_memeory_GB);
+                Console.WriteLine("Total Physical Memory :{0:#.###} GB", total_physical_memeory/ (1024 * 1024));
                 Console.WriteLine("Total Physical Memory :{0:#,###} MB", total_physical_memeory/1024);
-                Console.WriteLine("Total Physical Memory :{0:#,###} KB", info["TotalVisibleMemorySize"]);
+                Console.WriteLine("Total Physical Memory :{0:#,###} KB", total_physical_memeory);
 
+                Console.WriteLine("Free Physical Memory :{0:#,###} GB", free_physical_memeory/(1024 * 1024));
                 Console.WriteLine("Free Physical Memory :{0:#,###} MB", free_physical_memeory/1024);
-                Console.WriteLine("Free Physical Memory :{0:#,###} KB", info["FreePhysicalMemory"]);
+                Console.WriteLine("Free Physical Memory :{0:#,###} KB", free_physical_memeory);
 
-                Console.WriteLine("Memory Usage Percent = {0} %", 100 *(int) remain_physical_memory / (int)total_physical_memeory);
-                Console.WriteLine("DWConfig Usage Percent = {0:0.00} %", 100 * (memsize_MB / remain_physical_memory_MB) );
-                
-                Console.WriteLine("Remain Physical Memory : {0:0.00} GB", remain_physical_memory / (1024* 1024));
+                Console.WriteLine("Remain Physical Memory : {0:0.00} GB", remain_physical_memory / (1024 * 1024));
                 Console.WriteLine("Remain Physical Memory : {0:#,###} MB", remain_physical_memory / 1024);
                 Console.WriteLine("Remain Physical Memory : {0:#,###} KB", remain_physical_memory);
+
+                Console.WriteLine("Memory Usage Percent = {0} %", 100 *(int) remain_physical_memory / (int)total_physical_memeory);
+
+                //Console.WriteLine("DWConfig Usage Percent = {0:0.00} %", 100 * (memsize_MB / remain_physical_memory_MB) );
+                
+               
                 Console.WriteLine();
             }
 
         }
 
-        public static long GetProcessMemorySize(string processName)
-        {
-            PerformanceCounter performanceCounter = new PerformanceCounter("Process", "Working Set - Private", processName);
-
-            long memorySize = performanceCounter.RawValue;
-
-            return memorySize;
-        }
 
     }
 
